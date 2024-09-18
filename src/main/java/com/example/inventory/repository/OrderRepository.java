3bce1d97eeb2dc09b4fc0b7db4242171208cd428
Rajesh kumar <rajeshkumarmetta@gmail.com>
2024-09-18 12:22:50 +0530

    change files in repo

--- src/main/java/com/example/inventory/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.inventory.repository;
-
-import com.example.inventory.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
